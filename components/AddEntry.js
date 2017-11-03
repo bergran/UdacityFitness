16bda2e3aaebc5b7a5c45ce09056e3cb30928118
Ángel Berhó <bergraan@gmail.com>
2017-11-03 20:45:19 +0100

    Update db on AsyncStorage

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -6,6 +6,7 @@ import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
 import { Ionicons } from '@expo/vector-icons'
 import TextButton from './TextButton'
+import { submitEntry, removeEntry } from "../utils/api";
 
 
 function SubmitBtn ({ onPress }) {
@@ -66,7 +67,7 @@ export default class AddEntry extends Component {
 
         // Navigate to home
 
-        // Save to "DB"
+        submitEntry(entry, key)
 
         // Clear local notification
     }
@@ -78,7 +79,7 @@ export default class AddEntry extends Component {
 
         // Route to Home
 
-        // Update "DB"
+        removeEntry(key)
     }
 
     render() {
