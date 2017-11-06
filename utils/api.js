f91fa2381b600de80e8b63d2681be0ffd4a340bd
a.berho.medlab <a.berho@medlabmg.com>
2017-11-06 17:27:29 +0100

    feat: added History and fetch calendar results api

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,12 @@
 import { AsyncStorage } from 'react-native'
-import { CALENDAR_STORAGE_KEY } from './_calendar'
+import { formatCalendarResults, CALENDAR_STORAGE_KEY } from './_calendar'
 
-export function submitEntry ({entry, key}) {
+export function fetchCalendarResults () {
+    return AsyncStorage.getItem(CALENDAR_STORAGE_KEY)
+        .then(formatCalendarResults)
+}
+
+export function submitEntry ({ entry, key }) {
     return AsyncStorage.mergeItem(CALENDAR_STORAGE_KEY, JSON.stringify({
         [key]: entry
     }))
@@ -9,7 +14,7 @@ export function submitEntry ({entry, key}) {
 
 export function removeEntry (key) {
     return AsyncStorage.getItem(CALENDAR_STORAGE_KEY)
-        .then(results => {
+        .then((results) => {
             const data = JSON.parse(results)
             data[key] = undefined
             delete data[key]
