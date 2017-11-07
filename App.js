01f7c03690ac3ef363c235978a4d23323f05dcea
a.berho.medlab <a.berho@medlabmg.com>
2017-11-07 08:39:02 +0100

    feat: added UdaciFitness calendar to History, and History to App

--- App.js
+++ App.js
@@ -1,39 +1,21 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Text } from 'react-native'
 import AddEntry from './components/AddEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import entries from './reducers'
+import History from './components/History'
 
 export default class App extends React.Component {
     render() {
         return (
             <Provider store={createStore(entries)}>
                 <View style={{flex: 1}}>
-                    <AddEntry />
+                    <View><Text>Hello</Text></View>
+                    <View style={{height: 20}}/>
+                    <History />
                 </View>
             </Provider>
         )
     }
 }
-
-const styles = StyleSheet.create({
-    example: {
-        flex: 1,
-        flexWrap: 'wrap',
-        flexDirection: 'column-reverse'
-    },
-    container: {
-        flex: 1,
-        flexDirection: 'row',
-        flexWrap: 'nowrap',
-        justifyContent: 'space-around',
-        alignItems: 'center'
-    },
-    box: {
-        height: 100,
-        width: 100,
-        backgroundColor: '#e76e63',
-        margin: 10,
-    }
-})
