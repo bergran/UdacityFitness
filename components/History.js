3d27908a3aaf16aeff442b98b8179a86a9c3733c
a.berho.medlab <a.berho@medlabmg.com>
2017-11-07 15:20:49 +0100

    feat: added MetricCard to History MetricCards are containers to show metrics

--- components/History.js
+++ components/History.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native'
 import { connect } from 'react-redux'
 import { receiveEntries, addEntry } from '../actions'
 import { timeToString, getDailyReminderValue } from '../utils/helpers'
 import { fetchCalendarResults } from '../utils/api'
 import UdaciFitnessCalendar from 'udacifitness-calendar'
+import { white } from '../utils/colors'
+import DateHeader from './DateHeader'
+import MetricCard from './MetricCard'
+import { AppLoading } from 'expo'
 
 class History extends Component {
+
+    state = {
+        ready: false
+    }
+
     componentDidMount () {
         const { dispatch } = this.props
 
@@ -22,21 +31,38 @@ class History extends Component {
             .then(() => this.setState(() => ({ready: true})))
     }
     renderItem = ({ today, ...metrics }, formattedDate, key) => (
-        <View>
+        <View style={styles.item}>
             {today
-                ? <Text>{JSON.stringify(today)}</Text>
-                : <Text>{JSON.stringify(metrics)}</Text>}
+                ? <View>
+                    <DateHeader date={formattedDate}/>
+                    <Text style={styles.noDataText}>
+                        {today}
+                    </Text>
+                </View>
+                : <TouchableOpacity
+                    onPress={() => console.log('Pressed!')}
+                >
+                    <MetricCard date={formattedDate} metrics={metrics} />
+                </TouchableOpacity>}
         </View>
     )
     renderEmptyDate(formattedDate) {
         return (
-            <View>
-                <Text>No Data for this day</Text>
+            <View style={styles.item}>
+                <DateHeader date={formattedDate}/>
+                <Text style={styles.noDataText}>
+                    You didn't log any data on this day.
+                </Text>
             </View>
         )
     }
     render() {
         const { entries } = this.props
+        const { ready } = this.state
+
+        if (!ready) {
+            return (<AppLoading />)
+        }
 
         return (
             <UdaciFitnessCalendar
@@ -47,6 +73,30 @@ class History extends Component {
         )
     }
 }
+
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: white,
+        borderRadius: Platform.OS === 'ios' ? 16 : 2,
+        padding: 20,
+        marginLeft: 10,
+        marginRight: 10,
+        marginTop: 17,
+        justifyContent: 'center',
+        shadowRadius: 3,
+        shadowOpacity: 0.8,
+        shadowColor: 'rgba(0, 0, 0, 0.24)',
+        shadowOffset: {
+            width: 0,
+            height: 3
+        },
+    },
+    noDataText: {
+        fontSize: 20,
+        paddingTop: 20,
+        paddingBottom: 20
+    }
+})
 
 function mapStateToProps (entries) {
     return {
