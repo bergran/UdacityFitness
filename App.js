33113136721b4ea516955b66d3d01602e15d7268
a.berho.medlab <a.berho@medlabmg.com>
2017-11-07 16:48:55 +0100

    feat: Created tabs to navigate into app with fixed statusBar

--- App.js
+++ App.js
@@ -1,19 +1,66 @@
 import React from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import { View, Platform, StatusBar } from 'react-native'
+import { TabNavigator } from 'react-navigation'
 import AddEntry from './components/AddEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import entries from './reducers'
 import History from './components/History'
+import { white, purple } from './utils/colors';
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import { Constants } from 'expo'
+
+const Tabs = TabNavigator({
+    History: {
+        screen: History,
+        navigationOptions: {
+            tabBarLabel: 'History',
+            tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+        },
+    },
+    AddEntry: {
+        screen: AddEntry,
+        navigationOptions: {
+            tabBarLabel: 'Add Entry',
+            tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+        },
+    },
+}, {
+    navigationOptions: {
+        header: null
+    },
+    tabBarOptions: {
+        activeTintColor: Platform.OS === 'ios' ? purple : white,
+        style: {
+            height: 56,
+            backgroundColor: Platform.OS === 'ios' ? white : purple,
+            shadowColor: 'rgba(0, 0, 0, 0.24)',
+            shadowOffset: {
+                width: 0,
+                height: 3
+            },
+            shadowRadius: 6,
+            shadowOpacity: 1
+        }
+    }
+})
+
+function UdaciStatusBar ({backgroundColor, ...props}) {
+    return (
+        <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+            <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+        </View>
+    )
+}
 
 export default class App extends React.Component {
+
     render() {
         return (
             <Provider store={createStore(entries)}>
                 <View style={{flex: 1}}>
-                    <View><Text>Hello</Text></View>
-                    <View style={{height: 20}}/>
-                    <History />
+                    <UdaciStatusBar backgroundColor={purple} barStyle='light-content' />
+                    <Tabs />
                 </View>
             </Provider>
         )
